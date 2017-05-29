68d7ad276ea89724c76618f8b9859db86a625ae4
Nicolas Rougé <nicolas@nrouge.com>
2017-05-29 18:30:52 +0200

    Added license, gateway, Swagger on customer, and merged configuration and discovery services

--- configuration/src/main/java/com/nrouge/ms/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/com/nrouge/ms/configuration/ConfigurationApplication.java
@@ -3,9 +3,11 @@ package com.nrouge.ms.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer()
+@EnableConfigServer
+@EnableEurekaServer
 public class ConfigurationApplication {
 
 	public static void main(String[] args) {
--- customer/src/main/java/com/nrouge/ms/customer/CustomerApplication.java
+++ customer/src/main/java/com/nrouge/ms/customer/CustomerApplication.java
@@ -3,8 +3,14 @@ package com.nrouge.ms.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -12,6 +18,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 @RestController
 public class CustomerApplication {
 
@@ -27,5 +34,14 @@ public class CustomerApplication {
 	@GetMapping("/customers")
 	public List<Customer> allCustomers() {
 		return customers;
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 }
