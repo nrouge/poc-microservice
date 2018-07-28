240d5be62f6221f2044c918feb9ceada43eddecd
Nicolas Rougé <nicolas@nrouge.com>
2018-07-28 12:03:57 +0200

    Added common bootstrap configuration in the chassis

---
+++ chassis/src/main/java/com/nrouge/ms/chassis/ChassisBootstrapConfiguration.java
@@ -0,0 +1,9 @@
+package com.nrouge.ms.chassis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:com/nrouge/ms/chassis/bootstrap.yml")
+public class ChassisBootstrapConfiguration {
+}
